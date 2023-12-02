19e42e2f6e31aa1e943ad18473093a3da05bb177
RJJZitman <rogierz@hotmail.com>
2023-12-02 14:38:17 +0100

    mariadb class first version, docstrings missing

--- src/utils/mariadb.py
+++ src/utils/mariadb.py
@@ -1,35 +1,67 @@
-from typing import Dict, Optional, Any, List, Tuple, Union
+from typing import Optional, Any
 
-import mysql.connector
-from mysql.connector import Error
-from mysql.connector.abstracts import MySQLConnectionAbstract, MySQLCursorAbstract
 from sqlalchemy import create_engine
 from sqlalchemy.engine import Engine
+from sqlalchemy.engine.base import Connection, DBAPICursor
 
 
 class MariaDB:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, user: str, password: str, database: str, host: str = 'localhost', port: int = 3306) -> None:
+        self.user = user
+        self.password = password
+        self.database = database
+        self.host = host
+        self.port = port
+        self.connection: Optional[Connection] = None
+        self.cursor: Optional[DBAPICursor] = None
 
     def __enter__(self):
-        self.initiate_connection()
+        self._initiate_connection()
+        return self
 
     def __exit__(self, *exc_info):
-        self.close_connection()
+        self._close_connection()
 
-    def initiate_connection(self):
-        pass
+    def _initiate_connection(self):
+        # Use SQLAlchemy to create a connection to MariaDB
+        engine = self.engine_connect()
+        self.connection = engine.connect()
+        self.cursor = self.connection.connection.cursor()
 
-    def close_connection(self):
-        pass
+    def _close_connection(self):
+        if self.cursor:
+            self.cursor.close()
+        if self.connection:
+            self.connection.close()
 
-    def execute_query(self, query: str):
-        pass
+    def engine_connect(self) -> Engine:
+        # Use SQLAlchemy to create a MariaDB engine
+        connection_string = f"mysql+mysqlconnector://{self.user}:{self.password}@{self.host}:{self.port}/{self.database}"
+        engine = create_engine(connection_string)
+        return engine
 
-    def execute_query_select(self, query: str):
-        pass
+    def execute_query(self, query: str) -> None:
+        try:
+            self.cursor.execute(query)
+            self.connection.commit()
+        except Exception as e:
+            print(f"Error executing query: {e}")
+            self.connection.rollback()
 
-    def engine_connect(self):
-        pass
+    def execute_query_select(self, query: str) -> Any:
+        self.cursor.execute(query)
+        result = self.cursor.fetchall()
+        return result
+
+    def execute_sql_file(self, file_path: str) -> None:
+        with open(file=file_path, mode='r') as sql_file:
+            self.execute_query(query=sql_file.read())
 
 
+if __name__ == "__main__":
+    # Replace 'your_password' with the actual password for the 'Rogier' user
+    with MariaDB(user='Rogier', password='your_password', database='') as db:
+        print(db.execute_query_select("show databases;"))
+        db.execute_query("drop database if exists cellar;")
+        db.execute_sql_file(file_path='/Users/Lenna_C02ZL0UYLVDT/Weekeinden/cellar/src/sql/create_databases.sql')
+        print(db.execute_query_select("show databases;"))
